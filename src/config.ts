ce2b8148cb4d00b0eb075a443bd32b42faf36330
disco0 <disco0@users.noreply.github.com>
2020-12-20 14:19:26 -0800

    [src] Rework Config namespace + types + return typings for get

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,37 @@
-export interface HighlightDecoration extends vscode.DecorationRenderOptions
-{
-    [key: string]: any
-}
+/* CONFIGURATION TYPES */
 
-export interface HighlightRegexConfiguration
+namespace Config
 {
-    filterFileRegex?: string
-    filterLanguageRegex?: string
-    regexFlags?: string
-    decorations?: HighlightDecoration[];
-}
 
-export interface Configuration
-{
-    regexes: Record<string, HighlightRegexConfiguration>;
-    decorations: HighlightDecoration;
-    maxMatches?: number;
-    regexFlags?: ''
+    export interface HighlightDecoration extends vscode.DecorationRenderOptions
+    {
+        [key: string]: any
+    }
+
+    export interface HighlightRegexConfiguration
+    {
+        filterFileRegex?:     string
+        filterLanguageRegex?: string
+        regexFlags?:          string
+        decorations?:         HighlightDecoration[];
+    }
+
+    export interface Configuration
+    {
+        regexes:     Record<string, HighlightRegexConfiguration>;
+        decorations: HighlightDecoration;
+        maxMatches?: number;
+        regexFlags?: ''
+    }
+
+    /**
+     * Aliased form of settings shape for use with `vscode.workspace.getConfiguration().get(...`
+     */
+    export type NamespacedConfiguration =
+    {
+        [Key in keyof Configuration as `highlight.${Key}`]?: Configuration[Key]
+    } &  vscode.WorkspaceConfiguration
+
 }
 
 /* IMPORT */
@@ -25,16 +40,25 @@ import * as vscode from 'vscode';
 
 /* CONFIG */
 
-const Config = {
+namespace Config
+{
+    export const Namespace = 'highlight';
 
-  get ( extension = 'highlight' ) {
+    export const fallbackConfigurationObject = {} as Configuration;
 
-    return vscode.workspace.getConfiguration ().get<Configuration> ( extension );
+    export function get(): Config.Configuration
+    {
 
-  }
+        return vscode.workspace.getConfiguration ().get ( Namespace, fallbackConfigurationObject );
+
+    }
 
 };
 
 /* EXPORT */
 
 export default Config;
+
+export import Configuration               = Config.Configuration;
+export import HighlightDecoration         = Config.HighlightDecoration;
+export import HighlightRegexConfiguration = Config.HighlightRegexConfiguration;
